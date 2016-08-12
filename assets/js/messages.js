1f95fa3b60f62c0103e26776ea1d2dde5014921b
sakari jokinen <sakariij@gmail.com>
2016-08-13 01:13:23 +0300

    start of backwards loading

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -19,10 +19,74 @@ class Message extends React.Component {
   }
 }
 
+class Loader {
+  load(): Promise<(msgs: MessageCollection) => MessageCollection> {
+    throw new Error('dont call this')
+  }
+}
+
+class FromLatest extends Loader{
+  loading: Promise<(coll: MessageCollection) => MessageCollection>
+
+  constructor() {
+    super()
+  }
+
+  load() {
+    if (this.loading) {
+      return this.loading
+    }
+    this.loading = Promise.resolve(t => t)
+    return request.to(api.messages)
+      .then(response => collection => {
+        const currentPosition = collection.messages.findKey(row => row.type === 'loader' && row.loader === this)
+        if (currentPosition == null) {
+          return collection
+        }
+        return collection.splice(currentPosition, 1, List(response.messages.map(msg => ({ type: 'message', msg }))))
+      })
+  }
+}
+
+type Rows = List<{type: 'message', msg: api.Message} | {type: 'loader', loader: Loader }>
+
+class MessageCollection {
+  messages: Rows
+  size: number
+  observer: (sink: (coll: MessageCollection) => MessageCollection) => mixed
+
+  constructor(opts: { messages?: Rows, observer: (sink: (coll: MessageCollection) => MessageCollection) => mixed}) {
+    this.messages = opts.messages || List.of({ type: 'loader', loader: new FromLatest()})
+    this.size = this.messages.size
+    this.observer = opts.observer
+  }
+
+  splice(index, deleteCount, messages: Rows) {
+    return new MessageCollection({ messages: this.messages.splice(index, deleteCount, ...messages.toArray()), observer: this.observer})
+  }
+
+  load(startIndex: number, endIndex: number) {
+    for(var i = startIndex; i < endIndex; i++) {
+      const row = this.messages.get(i)
+      if (row.type === 'loader') {
+        row.loader.load().then(this.observer)
+      }
+    }
+  }
+
+  get(index: number): ?api.Message {
+    const row = this.messages.get(index)
+    if (row && row.type === 'message')
+      return row.msg
+  }
+}
+
 class MessageList extends React.Component {
   props: {
-    rows: List<api.Message>
+    rows: MessageCollection
   }
+
+  visibleRows: { overscanStartIndex: number, overscanStopIndex: number }
 
   state: {
     showLatest: boolean
@@ -30,7 +94,12 @@ class MessageList extends React.Component {
 
   constructor(props: mixed) {
     super(props)
+    this.visibleRows = { overscanStartIndex: 0, overscanStopIndex: 0 }
     this.state = { showLatest: true }
+  }
+
+  componentDidUpdate() {
+    this.load(this.visibleRows)
   }
 
   onScroll(position) {
@@ -42,6 +111,11 @@ class MessageList extends React.Component {
     return nextProps.rows !== this.props.rows
   }
 
+  load({overscanStartIndex, overscanStopIndex}) {
+    this.visibleRows =  {overscanStartIndex, overscanStopIndex }
+    this.props.rows.load(overscanStartIndex, overscanStopIndex + 1)
+  }
+
   render() {
     return <AutoSizer>
       {({width, height}) =>
@@ -49,11 +123,17 @@ class MessageList extends React.Component {
           scrollToIndex={this.state.showLatest ? this.props.rows.size - 1: undefined}
           scrollToAlignment="end"
           onScroll={scroll => this.onScroll(scroll)}
+          onRowsRendered={arg => this.load(arg)}
           width={width}
           height={height}
           rowHeight={20}
           rowCount={this.props.rows.size}
-          rowRenderer={({index}) => <Message message={this.props.rows.get(index)}/> }
+          rowRenderer={({index}) => {
+            const row = this.props.rows.get(index)
+            if (row)
+              return <Message message={row}/>
+            return <div>Loading</div>
+          }}
         />
       }
     </AutoSizer>
@@ -62,26 +142,20 @@ class MessageList extends React.Component {
 
 export default class Messages extends React.Component {
   state: {
-    messages: List<api.Message>,
+    messages: MessageCollection,
     inputValue: string
   }
 
   constructor(props: mixed) {
     super(props)
     this.state = {
-      messages: List(),
+      messages: new MessageCollection({ observer: fn => this.update(fn) }),
       inputValue: "",
     }
   }
-  componentWillMount() {
-    this.loadMessages()
-  }
 
-  loadMessages() {
-    request.to(api.messages)
-      .then(response => {
-        this.setState({messages: List(response.messages)})
-      })
+  update(fn: (coll: MessageCollection) => MessageCollection) {
+    this.setState({messages: fn(this.state.messages)})
   }
 
   sendMessage() {
@@ -89,8 +163,11 @@ export default class Messages extends React.Component {
       request.to(api.postMessage, { text: this.state.inputValue })
         .then(response => {
           console.log(response)
-          this.setState({inputValue: ''})
-          this.loadMessages()
+          this.setState({
+            inputValue: '',
+            messages: this.state.messages.splice(
+              this.state.messages.size, 0, List.of({ type: 'message', msg: response}))
+          })
         })
     }
   }
